c87839c4873a42a7c4f0b425db351b05a3656324
Joao <joaovitortgoliveira@gmail.com>
2025-09-11 21:11:34 -0300

    [Feat]: Ajuste na segurança

--- src/main/java/com/rotacerta/api/config/SecurityConfig.java
+++ src/main/java/com/rotacerta/api/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.rotacerta.api.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,7 +32,9 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(csrf -> csrf.disable())
+				.cors(Customizer.withDefaults())
 				.authorizeHttpRequests(auth -> auth
+						                       .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						                       .requestMatchers("/api/auth/**", "/api/payments/webhook").permitAll()
 						                       .anyRequest().hasRole("USER")
 				                      )
@@ -45,7 +49,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
